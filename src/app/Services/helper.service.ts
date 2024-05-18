a74f7ee7af0f25a15fa54b7817605004c6900fe0
MoathTar <moath.altarawneh@arizonaais.com>
2024-05-18 20:11:40 +0300

    added constraints for the inputs

--- src/app/Services/helper.service.ts
+++ src/app/Services/helper.service.ts
@@ -1,22 +1,38 @@
 import { Injectable } from '@angular/core';
 import { CORE_SIZE } from './constants';
+import { QuranService } from './quran.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HelperService {
 
-  constructor() { }
+  constructor(private quranService:QuranService) { }
 
 
-  inputNumberRestrict(input:HTMLInputElement,min:number,max:number){
-    let numVal = Number.parseInt(input.value);
-    if(min > numVal){
-      input.value = min.toString();
+  inputNumberRestrict(surahInput:HTMLInputElement,startAyahInput:HTMLInputElement,endAyahInput:HTMLInputElement,min:number,max:number){
+    if(!surahInput.value || surahInput.value == ''){return}
+    let surahNumber = Number.parseInt(surahInput.value);
+    if(min > surahNumber){
+      surahInput.value = min.toString();
     }
-    if(numVal > max){
-      input.value = max.toString();
+    if(surahNumber > max){
+      surahInput.value = max.toString();
     }
+    startAyahInput.disabled = true;
+    endAyahInput.disabled = true;
+    startAyahInput.value = '';
+    endAyahInput.value = '';
+    this.quranService.GetSurah(surahNumber)?.subscribe(surah => {
+      startAyahInput.value = '1';
+      endAyahInput.value = surah.totalAyah.toString();
+      startAyahInput.max = surah.totalAyah.toString();
+      endAyahInput.max = surah.totalAyah.toString();
+      startAyahInput.disabled = false;
+      endAyahInput.disabled = false;
+    });
+
+
   }
 
   getDownloadProgress(url:string,recieved:number):number{
