b7eeb692767f18ee3e6d2eba69323709e6afe90a
MoathTar <moath.altarawneh@arizonaais.com>
2024-05-29 09:57:44 +0300

    test mt 2

--- src/app/GeneratorPage/generator/generator.component.ts
+++ src/app/GeneratorPage/generator/generator.component.ts
@@ -7,7 +7,7 @@ import { Surah } from 'src/app/Interfaces/surah';
 import { HelperService } from 'src/app/Services/helper.service';
 import { QuranService } from 'src/app/Services/quran.service';
 
-const baseURL = "https://unpkg.com/@ffmpeg/core@0.12.6/dist/esm";
+const baseURL = "https://unpkg.com/@ffmpeg/core-mt@0.12.6/dist/esm";
 export interface currentLoading{
   name:string;
   value:number;
